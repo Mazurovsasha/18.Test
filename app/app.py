fe84917aac342995103d3260da0ceb5d1d8042ca
Alexsandr Mazurov <mazurovsasha@gmail.com>
2023-12-15 15:59:55 +0000

    test 5

--- app/app.py
+++ app/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#     name = request.form.get('name')
+#     return render_template('hello.html', name=name)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# Создаем экземпляр приложения
 app = Flask(__name__)
 
+# Определяем маршрут для главной страницы
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/hello', methods=['POST'])
 def hello():
-    name = request.form.get('name')
-    return render_template('hello.html', name=name)
+    return "Домашнее задание 18.Test Мазуров Александр !"
 
+# Запускаем сервер
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
