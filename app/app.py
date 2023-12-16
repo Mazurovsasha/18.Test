b040bbebc0d4276bd3d8c4318462b33748fe5674
Alexsandr Mazurov <mazurovsasha@gmail.com>
2023-12-16 15:47:30 +0000

    test web

--- app/app.py
+++ app/app.py
@@ -1,28 +1,16 @@
 from flask import Flask, render_template, request
 
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/hello', methods=['POST'])
-# def hello():
-#     name = request.form.get('name')
-#     return render_template('hello.html', name=name)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# Создаем экземпляр приложения
 app = Flask(__name__)
 
-# Определяем маршрут для главной страницы
 @app.route('/')
-def hello():
-    return "Домашнее задание 18.Test Мазуров Александр !"
+def index():
+    return render_template('index.html')
 
-# Запускаем сервер
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form.get('name')
+    return render_template('hello.html', name=name)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
